701c62c08647d1bf758a79bc88b2d8c84e54c803
Arturo Herrero <arturo.herrero@gmail.com>
2013-03-01 09:49:14 +0100

    Fixed indentation. Groovyism: delete getClass method

--- src/main/groovy/es/osoco/transformations/NotASTTransformation.groovy
+++ src/main/groovy/es/osoco/transformations/NotASTTransformation.groovy
@@ -13,25 +13,25 @@ import java.lang.annotation.*
 @GroovyASTTransformation(phase = CompilePhase.CONVERSION)
 public class NotASTTransformation implements ASTTransformation {
 
-   public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
-       List classes = sourceUnit.ast?.classes
-       MethodNode notMethod = notMethod()
-       classes.each { ClassNode clazz -> clazz.addMethod(notMethod) }  
-   }
-   
-   private MethodNode notMethod() {
-       ASTNode[] ast = new AstBuilder().buildFromSpec {
-           method('not', ACC_PUBLIC | ACC_STATIC, Boolean.class) {
-               parameters {
-                   parameter 'expression': Object.class
-               }
-               exceptions {}
-               block {
-                   owner.expression.addAll new AstBuilder().buildFromCode { !expression }
-               }      
-               annotations {}
-           }
-       }
-       ast.first()
-   }
+    public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
+        List classes = sourceUnit.ast?.classes
+        MethodNode notMethod = notMethod()
+        classes.each { ClassNode clazz -> clazz.addMethod(notMethod) }
+    }
+
+    private MethodNode notMethod() {
+        ASTNode[] ast = new AstBuilder().buildFromSpec {
+            method('not', ACC_PUBLIC | ACC_STATIC, Boolean) {
+                parameters {
+                    parameter 'expression': Object
+                }
+                exceptions {}
+                block {
+                    owner.expression.addAll new AstBuilder().buildFromCode { !expression }
+                }
+                annotations {}
+            }
+        }
+        ast.first()
+    }
 }
